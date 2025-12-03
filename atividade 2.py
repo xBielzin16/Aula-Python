6708eb78eddec2ddfded12309678f0cecf9b73e6
xBielzin16 <gab.jr@hotmail.com>
2025-12-03 13:32:34 -0300

    salvar

--- atividade 2.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system ("cls")
-
-idade = int(input("Digite sua idade: "))
-
-if idade >= 18 and idade <= 65:
-    print("Obrigado a votar.")
-else:
-    print("Não é obrigado a votar!")
